a442df4909ebe80d32613e31cf1b2fae73d41a30
Jenny Ma <jennygzma@gmail.com>
2025-02-18 19:00:44 -0500

    version robin

---
+++ frontend/src/pages/current/components/config_creation.tsx
@@ -0,0 +1,113 @@
+import { Card, Paper, Stack, Tab, Tabs, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useAppContext } from "../hooks/app-context";
+import Button from "../../../components/Button";
+import TextField from "../../../components/TextField";
+import { SERVER_URL } from "..";
+
+const ConfigCreation = () => {
+  const { updateIsLoading, currentPrototype } = useAppContext();
+  const [config, setConfig] = useState("");
+  const [updatedConfig, setUpdatedConfig] = useState(false);
+
+  const saveConfig = () => {
+    updateIsLoading(true);
+    axios({
+      method: "POST",
+      url: `${SERVER_URL}/save_config`,
+      data: {
+        config,
+      },
+    })
+      .then((response) => {
+        console.log("/save_config request successful:", response.data);
+        getConfig();
+        setUpdatedConfig(false);
+      })
+      .catch((error) => {
+        console.error("Error calling /save_config request:", error);
+      })
+      .finally(() => {
+        updateIsLoading(false);
+      });
+  };
+
+  const getConfig = () => {
+    updateIsLoading(true);
+    axios({
+      method: "GET",
+      url: `${SERVER_URL}/get_config`,
+    })
+      .then((response) => {
+        console.log("/get_config request successful:", response.data);
+        setConfig(response.data.code);
+      })
+      .catch((error) => {
+        console.error("Error calling /get_config request:", error);
+      })
+      .finally(() => {
+        updateIsLoading(false);
+      });
+  };
+
+  const generateConfig = () => {
+    updateIsLoading(true);
+    axios({
+      method: "POST",
+      url: `${SERVER_URL}/generate_config`,
+    })
+      .then((response) => {
+        console.log("/generate_config request successful:", response.data);
+        getConfig();
+      })
+      .catch((error) => {
+        console.error("Error calling /generate_config request:", error);
+      })
+      .finally(() => {
+        updateIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    if (!currentPrototype) return;
+    getConfig();
+  }, [currentPrototype]);
+
+  // if (!currentPrototype) return <></>;
+  return (
+    <Stack spacing="10px" width="100%">
+      <Button
+        onClick={generateConfig}
+        sx={{
+          width: "100%",
+        }}
+      >
+        {config ? "Regenerate Config" : "Generate Config"}
+      </Button>
+      {config && (
+        <Stack spacing="10px">
+          <TextField
+            className={"code"}
+            rows={100}
+            value={config}
+            onChange={(e) => {
+              setConfig(e.target.value);
+              setUpdatedConfig(true);
+            }}
+            code={true}
+          />
+          <Button
+            disabled={!updatedConfig}
+            onClick={saveConfig}
+            sx={{ width: "100%" }}
+          >
+            Update Config
+          </Button>
+        </Stack>
+      )}
+    </Stack>
+  );
+};
+
+export default ConfigCreation;
